5faba7b3f74b58e65cafcbc2b9d763ba2be957cf
Arlieutenant007 <jethvatushar87@gmail.com>
2023-05-17 21:42:38 +0530

    link

--- lib/constants/all_constant.dart
+++ lib/constants/all_constant.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:image_picker/image_picker.dart';
 
-String url = "http://192.168.159.230:3000";
+String url = "https://fcb-donation.onrender.com";
 
 class GlobalSnakbar {
   static GlobalKey<ScaffoldMessengerState> key =
